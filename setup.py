b33e99ab1ea89999cb055cc6b48772ff05a4bae5
Aritri96 <154794391+Aritri96@users.noreply.github.com>
2024-11-20 21:44:17 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="elastiq",
-    version="0.1",
-    packages=find_packages(),
+    name="elastiq",  # Your package name
+    version="1.0.0",  # Version of your package
+    description="A Python-based tool for calculating elastic constants using stress and strain data",
+    author="Your Name",
+    author_email="your_email@example.com",
+    url="https://github.com/Aritri96/ElastiQ",  # Repository URL
+    license="MIT",  # Choose a license (e.g., MIT, Apache-2.0, etc.)
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[
+        "numpy",  # Add dependencies here
+    ],
     entry_points={
-        'console_scripts': [
-            'elastiq=elastiq.main:main',
+        "console_scripts": [
+            "elastiq=elastiq.main:main",  # Replace with your module and main function
         ],
     },
-    install_requires=[
-        'numpy',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    description="A tool for calculating elastic constants of materials.",
-    author="Roy",
-    license="NA",
+    python_requires=">=3.6",  # Minimum Python version
 )
